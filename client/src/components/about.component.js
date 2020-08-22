41eef1c0de1953a75d97cfe6a496090bf004fc78
Anju Gopalkrishnan <anjugk.26@gmail.com>
2020-08-22 21:15:59 +0530

    fixed mailto

--- client/src/components/about.component.js
+++ client/src/components/about.component.js
@@ -11,7 +11,7 @@ export default class Hotspots extends Component {
                 <br /><br /><br />
                 <h4>Contact</h4>
               Anju Gopalkrishnan<br /><br />
-                <span style={{ padding: "10px" }}>  <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=navimumbaicovid19@gmail.com" target="_blank" alt="mail"><i class="fa fa-envelope fa-3x" style={{ color: "#222222" }}></i></a></span>
+                <span style={{ padding: "10px" }}>  <a href="mailto:navimumbaicovid19@gmail.com" target="_blank" alt="mail"><i class="fa fa-envelope fa-3x" style={{ color: "#222222" }}></i></a></span>
                 <span style={{ padding: "10px" }}>  <a href="https://www.linkedin.com/in/anjugopalkrishnan" target="_blank"><i class="fa fa-linkedin-square fa-3x" style={{ color: "#0073b0" }}></i> </a></span>
                 <span style={{ padding: "10px" }}>  <a href="https://www.reddit.com/user/-Anju-" target="_blank"><i class="fa fa-reddit-alien fa-3x" style={{ color: "#ff4500" }}></i></a></span>
                 <br /><br /><br />
